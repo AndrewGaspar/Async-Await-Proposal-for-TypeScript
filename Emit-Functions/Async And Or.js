934b1583b0a810f4c23908f7eea712749ff1cf52
Andrew Gsapar <andrew.james.gaspar@gmail.com>
2013-04-25 02:25:26 -0500

    Create Async And Or.js

---
+++ Emit-Functions/Async And Or.js
@@ -0,0 +1,15 @@
+// AND:
+function __and(b1, b2) {
+  return __promisify(b1()).then(function(_0) {
+    if(_0) return b2(); // will return a promise whether or not b2 returns a promise
+    else return _0;
+  });
+}
+
+// OR:
+function __or(b1, b2) {
+  return __promisify(b1()).then(function(_0){
+    if(_0) return _0;
+    else return b2(); // will return a promise whether or not b2 returns a promise
+  });
+}
