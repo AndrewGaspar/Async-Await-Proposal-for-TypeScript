67cdadc67c1bb3da38bac91ae72795e18174b4af
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-04-25 21:50:06 -0500

    Made changes to ifelse.js that make it more efficient and less verbose.

--- Emit-Functions/ifelse.js
+++ Emit-Functions/ifelse.js
@@ -1,33 +1,27 @@
 function __ifElse(conditionals, continuation) {
-    // If there are no conditions left to check, run the continuation and return a promise for it.
-    if (!conditionals.length) return __promisify(continuation());
-
-    var ifBlock = conditionals[0]; // get first if block
-
-    // if there is only one left and there is no condition on the block, immediately run the body
-    // this would be an else block
-    if (conditionals.length === 1 && !ifBlock.condition) return __promisify(ifBlock.body());
-
-    // invoke the condition
-    var condition = ifBlock.condition();
-
     function checkCondition(truthy) {
         //  If the condition evaluates to true, run the body of the function then skip to the 
         //  continuation. otherwise call __ifElse recursively with the first item dequeued.
 
         if (truthy) {
-            return __promisify(ifBlock.body()).then(function (value) {
-                //  If this block contained a return statement, the return value of the body
-                //  should be returned. Otherwise the continuation promise should be returned.
-                return (ifBlock.shouldReturn) ? value : __promisify(continuation());
-            });
-        }
-        else return __ifElse(conditionals.slice(1, conditionals.length), continuation);
+            var bodyEval = ifBlock.body();
+            return (__isPromise(bodyEval)) ? bodyEval.then(returnPromise) : returnPromise();
+        } else return __ifElse(conditionals.slice(1, conditionals.length), continuation);
     }
 
-    if (__isPromise(condition)) {
-        return condition.then(checkCondition); // wait for the condition to resolve, then evaluate it
-    } else {
-        return checkCondition(condition); // run function immediately.
+    function returnPromise(value) {
+        //  If ifBlock contained a return statement or there is no continuation, the return value of the body
+        //  should be returned. Otherwise the continuation promise should be returned.
+        return __promisify((ifBlock.shouldReturn || !continuation) ? value : continuation());
     }
+    // If there are no conditions left to check, run the continuation and return a promise for it.
+    if (!conditionals.length) return __promisify(continuation());
+
+    var ifBlock = conditionals[0]; // get first if block
+
+    // If there is no condition and its the last block in if-else, it is an else block.
+    // Condition will be true in this case. Otherwise it is the evaluation of the block's condition.
+    var condition = (conditionals.length === 1 && !ifBlock.condition) || ifBlock.condition();
+
+    return (__isPromise(condition)) ? condition.then(checkCondition) : checkCondition(condition);
 }
