895428cd0422d3541caf25bdc7d47a08167825e4
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-04-29 01:59:22 -0500

    Additional changes to loop.js

--- Emit-Functions/loop.js
+++ Emit-Functions/loop.js
@@ -1,12 +1,18 @@
-function __loop(conditional, continuation, parentReturn) {
+function __loop(conditional, continuation, parentReturn, isDo) {
     var returning = false,
         returnValue;
+
+    function continuing() {
+        return __promisify((returning) ? returnValue : next());
+    }
 
     function next() {
         var conditionEval = skipConditionEval || loop.condition();
         skipConditionEval = false;
 
         return __promisify(conditionEval).then(function (truthy) {
+
+
             if (truthy) {
                 if(loop.body) {
                     var bodyEval = loop.body(function (value) {
@@ -14,14 +20,13 @@ function __loop(conditional, continuation, parentReturn) {
                         returnValue = value;
                         returning = true;
                     });
-
-
+                    
                 }
             } else return __promisify(continuation());
         });
     }
 
-    var skipConditionEval = !!loop.do;
+    var skipConditionEval = !!isDo;
 
     return next();
 }
