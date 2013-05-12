079d3813bf4db613b54b8058aaa41c84bcd8e838
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-05-12 17:48:10 -0500

    Changed control to more closely match how it would be emit to compile TypeScript file.

--- Emit-Functions/control.js
+++ Emit-Functions/control.js
@@ -1,32 +1,34 @@
-function ControlBlock(parentControl, dontInherit) {
-    this.parent = parentControl || {};
-    this.dontInherit = dontInherit || {};
-    this.shouldReturn = false;
-    this.shouldBreak = false;
-    this.shouldContinue = false;
-    this.continueExecuting = true;
-}
-
-ControlBlock.prototype = {
-    __return: function (value) {
-        if (this.parent.__return && !this.dontInherit.__return) this.parent.__return(value);
-        this.returnValue = value;
-        this.shouldReturn = true;
-        this.continueExecuting = false;
-    },
-    __break: function () {
-        if (this.parent.__break && !this.dontInherit.__break) this.parent.__break();
-        this.shouldBreak = true;
-    },
-    __continue: function () {
-        if (this.parent.__continue && !this.dontInherit.__continue) this.parent.__continue();
-        this.shouldContinue = true;
-        this.continueExecuting = false;
+var __getControlBlock = this.__getControlBlock || (function () { 
+    function ControlBlock(parentControl, dontInherit) {
+        this.parent = parentControl || {};
+        this.dontInherit = dontInherit || {};
+        this.shouldReturn = false;
+        this.shouldBreak = false;
+        this.shouldContinue = false;
+        this.continueExecuting = true;
     }
-}
 
-function getControlBlock(parentControl, dontInherit) {
-    return new ControlBlock(parentControl, dontInherit);
-}
+    ControlBlock.prototype = {
+        __return: function (value) {
+            if (this.parent.__return && !this.dontInherit.__return) this.parent.__return(value);
+            this.returnValue = value;
+            this.shouldReturn = true;
+            this.continueExecuting = false;
+        },
+        __break: function () {
+            if (this.parent.__break && !this.dontInherit.__break) this.parent.__break();
+            this.shouldBreak = true;
+        },
+        __continue: function () {
+            if (this.parent.__continue && !this.dontInherit.__continue) this.parent.__continue();
+            this.shouldContinue = true;
+            this.continueExecuting = false;
+        }
+    }
 
-module.exports = getControlBlock;
+    return function (parentControl, dontInherit) {
+        return new ControlBlock(parentControl, dontInherit);
+    }
+})();
+
+module.exports = __getControlBlock;
