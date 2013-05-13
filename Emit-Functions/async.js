107dffdb72cc4e6397d13c0b82c8ef5fed12c894
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-05-13 14:33:41 -0500

    Async argument name changed to be more descriptive.

--- Emit-Functions/async.js
+++ Emit-Functions/async.js
@@ -1,11 +1,11 @@
 var __defer = this.__defer || require("./defer");
 
-var __async = this.__async || function (f) {
+var __async = this.__async || function (asyncFunction) {
     return function () {
         var def = __defer();
         
         try {
-            def.resolve(f.apply(this, arguments));
+            def.resolve(asyncFunction.apply(this, arguments));
         } catch (e) {
             def.reject(e);
         }
