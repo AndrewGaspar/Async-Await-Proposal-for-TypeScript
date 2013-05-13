30baabb2795594813d44c6efb23d02a7da1ff286
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-05-13 15:41:13 -0500

    Updated try catch tests to match new async pattern.

--- Emit-Functions/test/tryCatch-test.js
+++ Emit-Functions/test/tryCatch-test.js
@@ -8,18 +8,20 @@ describe("synchronous", function () {
     it("should end execution early", function (done) {
         var a, b, c;
 
-        __async(function () {
-            return __tryCatch({
-                __try: function () {
-                    throw new Error(true);
-                    a = true;
-                },
-                __catch: function (e) {
-                    b = e.message;
-                },
-                __finally: function () {
-                    c = true;
-                }
+        (function () {
+            return __async(function () {
+                return __tryCatch({
+                    __try: function () {
+                        throw new Error(true);
+                        a = true;
+                    },
+                    __catch: function (e) {
+                        b = e.message;
+                    },
+                    __finally: function () {
+                        c = true;
+                    }
+                });
             });
         })().then(function () {
             done((typeof a === "undefined" && b === "true" && c === true) ? undefined : "Not correct execution - " + a + "," + b + "," + c);
@@ -31,27 +33,29 @@ describe("synchronous", function () {
     it("should catch from if", function (done) {
         var a, b, c, d, e;
 
-        __async(function () {
-            return __tryCatch({
-                __try: function () {
-                    a = true;
-                    return __ifElse([{
-                        condition: function () {
-                            return true;
-                        },
-                        body: function () {
-                            throw new Error("da");
-                            b = true;
-                        }
-                    }], function () {
-                        c = true;
-                    });
-                },
-                __catch: function (e) {
-                    if (e.message === "da") d = true;
-                }
-            }, function () {
-                e = true;
+        (function () {
+            return __async(function () {
+                return __tryCatch({
+                    __try: function () {
+                        a = true;
+                        return __ifElse([{
+                            condition: function () {
+                                return true;
+                            },
+                            body: function () {
+                                throw new Error("da");
+                                b = true;
+                            }
+                        }], function () {
+                            c = true;
+                        });
+                    },
+                    __catch: function (e) {
+                        if (e.message === "da") d = true;
+                    }
+                }, function () {
+                    e = true;
+                });
             });
         })().then(function () {
             done((a && !b && !c && d && e) ? undefined : new Error("Derp - " + a + b + c + d + e));
@@ -59,17 +63,19 @@ describe("synchronous", function () {
     });
 
     it("should reject with throw in finally", function (done) {
-        __async(function () {
-            return __tryCatch({
-                __try: function () {
-                    throw 1;
-                },
-                __catch: function () {
-                    throw 2;
-                },
-                __finally: function () {
-                    throw 3;
-                }
+        (function () {
+            return __async(function () {
+                return __tryCatch({
+                    __try: function () {
+                        throw 1;
+                    },
+                    __catch: function () {
+                        throw 2;
+                    },
+                    __finally: function () {
+                        throw 3;
+                    }
+                });
             });
         })().then(function () {
             done(new Error("should have been rejected"));
@@ -79,18 +85,20 @@ describe("synchronous", function () {
     });
 
     it("should continue", function (done) {
-        __async(function () {
-            var a;
+        (function () {
+            return __async(function () {
+                var a;
 
-            return __tryCatch({
-                __try: function () {
-                    throw 1;
-                },
-                __catch: function (e, _c) {
-                    a = e + 1;
-                }
-            }, function () {
-                return a + 1;
+                return __tryCatch({
+                    __try: function () {
+                        throw 1;
+                    },
+                    __catch: function (e, _c) {
+                        a = e + 1;
+                    }
+                }, function () {
+                    return a + 1;
+                });
             });
         })().then(function (val) {
             done(val === 3 ? undefined : new Error("Error: " + val));
@@ -102,18 +110,20 @@ describe("asynchronous", function () {
     it("should end execution early", function (done) {
         var a, b, c;
 
-        __async(function () {
-            return __tryCatch({
-                __try: function () {
-                    return __rejectify(new Error(true));
-                    a = true;
-                },
-                __catch: function (e) {
-                    b = e.message;
-                },
-                __finally: function () {
-                    c = true;
-                }
+        (function () {
+            return __async(function () {
+                return __tryCatch({
+                    __try: function () {
+                        return __rejectify(new Error(true));
+                        a = true;
+                    },
+                    __catch: function (e) {
+                        b = e.message;
+                    },
+                    __finally: function () {
+                        c = true;
+                    }
+                });
             });
         })().then(function () {
             done((typeof a === "undefined" && b === "true" && c === true) ? undefined : "Not correct execution - " + a + "," + b + "," + c);
@@ -125,19 +135,21 @@ describe("asynchronous", function () {
     it("should rethrow exception", function (done) {
         var a, b, c;
 
-        __async(function () {
-            return __tryCatch({
-                __try: function () {
-                    return __rejectify(new Error(true));
-                    a = true;
-                },
-                __catch: function (e) {
-                    throw e;
-                    b = e.message;
-                },
-                __finally: function () {
-                    c = true;
-                }
+        (function () {
+            return __async(function () {
+                return __tryCatch({
+                    __try: function () {
+                        return __rejectify(new Error(true));
+                        a = true;
+                    },
+                    __catch: function (e) {
+                        throw e;
+                        b = e.message;
+                    },
+                    __finally: function () {
+                        c = true;
+                    }
+                });
             });
         })().then(function () {
             done("should have thrown error");
