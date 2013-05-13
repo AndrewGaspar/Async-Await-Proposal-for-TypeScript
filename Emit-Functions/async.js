be7eb1e01bcd5909f6bc2b9e63faebe76799434b
Andrew Gaspar <andrew.james.gaspar@gmail.com>
2013-05-13 14:33:08 -0500

    Now applies arguments to function of async.

--- Emit-Functions/async.js
+++ Emit-Functions/async.js
@@ -3,9 +3,9 @@ var __defer = this.__defer || require("./defer");
 var __async = this.__async || function (f) {
     return function () {
         var def = __defer();
-
+        
         try {
-            def.resolve(f());
+            def.resolve(f.apply(this, arguments));
         } catch (e) {
             def.reject(e);
         }
